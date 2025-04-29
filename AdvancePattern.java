f1d030c5d827a1eb0394289f41e60f91e4cccb6b
Muneeb-Akbar <muneeb.akar09@gmail.com>
2025-04-30 02:24:34 +0500

    practice some advance stars patterns

---
+++ AdvancePattern.java
@@ -0,0 +1,50 @@
+class AdvancePattern{
+
+
+    public static void ButterflyPattern() {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            for(int j=0; j<2*(5-i-1); j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        for(int i=5; i>0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            for(int j=0; j<2*(5-i); j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void Rhombus(){
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5-i-1;j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=0; j<5; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+       Rhombus();
+    }
+}
